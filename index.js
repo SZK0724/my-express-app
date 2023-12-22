806626ed4b8f1a9db0b557b5f67d5d290e5e8f77
SAM ZHI KANG <b022120028@student.utem.edu.my>
2023-12-23 00:59:14 +0800

    Your commit message here

--- index.js
+++ index.js
@@ -1,51 +1,33 @@
 const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsDoc = require('swagger-jsdoc');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Swagger configuration
-const swaggerOptions = {
-    definition: {
-        openapi: '3.0.0',
-        // ... other options ...
-    },
-    apis: ['./routes/*.js'], // Make sure this path is correct
-};
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+// MongoDB URI. In production, this will be set in Azure's Application Settings.
+const uri = process.env.MONGODB_URI || "mongodb+srv://zhikangsam0724:2Un24f6Hfk4l1Z1x@cluster0.1jh2xph.mongodb.net/";;
 
-// MongoDB client setup
-const uri = process.env.MONGODB_URI; // Use environment variable for URI
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-
-// Connect to MongoDB
-client.connect()
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch(err => {
-        console.error('Failed to connect to MongoDB', err);
-    });
 
 app.use(express.json());
 
-// Routes
-const helloRoutes = require('./routes/hello'); // Ensure this file exists and is correctly implemented
-app.use(helloRoutes);
+client.connect(err => {
+  if (err) {
+    console.error('Database connection failed', err);
+    process.exit();
+  }
+  console.log('Connected to MongoDB');
 
-app.get('/', (req, res) => {
+  // Define routes here
+  app.get('/', (req, res) => {
     res.send('Hello World!');
-});
+  });
 
-app.listen(port, () => {
+  // Start the Express server
+  app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+  });
 });
