b3e5a789bc2047c0a0006e083024e45e265216e3
SAM ZHI KANG <b022120028@student.utem.edu.my>
2023-12-22 22:56:24 +0800

    Your commit message here

--- index.js
+++ index.js
@@ -1,56 +1,51 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Swagger configuration
 const swaggerOptions = {
     definition: {
-      openapi: '3.0.0',
-      // ... other options ...
-      components: {
-        securitySchemes: {
-          bearerAuth: {
-            type: 'http',
-            scheme: 'bearer',
-            bearerFormat: 'JWT',
-          },
-        },
-      },
+        openapi: '3.0.0',
+        // ... other options ...
     },
-  
-  apis: ['./routes/*.js'], // path to API docs
+    apis: ['./routes/*.js'], // Make sure this path is correct
 };
-
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://zhikangsam0724:2Un24f6Hfk4l1Z1x@cluster0.1jh2xph.mongodb.net/";
-
+// MongoDB client setup
+const uri = process.env.MONGODB_URI; // Use environment variable for URI
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
-client.connect().then(res => {
-  console.log(res);
-});
+// Connect to MongoDB
+client.connect()
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('Failed to connect to MongoDB', err);
+    });
 
 app.use(express.json());
 
+// Routes
+const helloRoutes = require('./routes/hello'); // Ensure this file exists and is correctly implemented
+app.use(helloRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
-
-const helloRoutes = require('./routes/hello');
-app.use(helloRoutes);
