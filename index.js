9d91a8275780f868f8a77c2888b32a0b3cb60ace
SAM ZHI KANG <b022120028@student.utem.edu.my>
2023-12-24 20:06:19 +0800

    Your commit message here

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json())
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Hello World API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./routes/*.js'], // path to API docs
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get('/', (req, res) => {
-   res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-   console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
+
+const helloRoutes = require('./routes/hello');
+app.use(helloRoutes);
