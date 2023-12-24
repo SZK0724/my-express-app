7638343d1452a5d2ba75ac69df108c57429204c7
SAM ZHI KANG <b022120028@student.utem.edu.my>
2023-12-24 18:51:21 +0800

    Your commit message here

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./package.json');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
 
-const app = express();
-const MONGODB_URI="mongodb+srv://zhikangsam0724:2Un24f6Hfk4l1Z1x@cluster0.1jh2xph.mongodb.net/";
-// Connect to MongoDB
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+app.use(express.json())
 
-// BodyParser Middleware
-app.use(bodyParser.json());
-
-// Swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// Define routes here
 app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+   res.send('Hello World!')
+})
 
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+   console.log(`Example app listening on port ${port}`)
+})
